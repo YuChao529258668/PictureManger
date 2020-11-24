b2bb42d3fa8e3a244ae186189e29e08e198b7808
YuChao529258668 <529258668@qq.com>
2020-11-24 12:55:41 +0800

    整理暂时不用的方法

--- YCPictureManager/YCPictureManager/Lib/YCImageCompare.mm
+++ YCPictureManager/YCPictureManager/Lib/YCImageCompare.mm
@@ -32,14 +32,6 @@
     return scaledImage;
 }
 
-// 获取相似度
-+ (float)isImageFloat:(UIImage *)image1 likeImage:(UIImage *)image2 {
-    IplImage *iplimage1 = [self convertToIplImage:image1];
-    IplImage *iplimage2 = [self convertToIplImage:image2];
-    double sililary = [self ComparePPKHist:iplimage1 withParam2:iplimage2];
-    return sililary;
-}
-
 // 比较
 + (double)ComparePPKHist:(IplImage *)srcIpl withParam2:(IplImage *)srcIpl1 {
     if (srcIpl->width==srcIpl1->width && srcIpl->height==srcIpl1->height) {
@@ -66,6 +58,142 @@
     }
 
     return 1.f;
+}
+
+
+// 多通道彩色图片的直方图比对
++ (double)CompareHist:(IplImage*)image1 withParam2:(IplImage*)image2 {
+    int hist_size = 256;
+    IplImage *gray_plane = cvCreateImage(cvGetSize(image1), 8, 1);
+    cvCvtColor(image1, gray_plane, CV_BGR2GRAY);
+    
+    CvHistogram *gray_hist = cvCreateHist(1, &hist_size, CV_HIST_ARRAY);
+    cvCalcHist(&gray_plane, gray_hist);
+    
+    IplImage *gray_plane2 = cvCreateImage(cvGetSize(image2), 8, 1);
+    cvCvtColor(image2, gray_plane2, CV_BGR2GRAY);
+    CvHistogram *gray_hist2 = cvCreateHist(1, &hist_size, CV_HIST_ARRAY);
+    cvCalcHist(&gray_plane2, gray_hist2);
+
+    return cvCompareHist(gray_hist, gray_hist2, CV_COMP_BHATTACHARYYA);
+}
+
+
+// UIImage类型转换为IPlImage类型
++ (IplImage*)convertToIplImage:(UIImage*)image {
+    CGImageRef imageRef = image.CGImage;
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    IplImage *iplImage = cvCreateImage(cvSize(image.size.width, image.size.height), IPL_DEPTH_8U, 4);
+    CGContextRef contextRef = CGBitmapContextCreate(iplImage->imageData, iplImage->width, iplImage->height, iplImage->depth, iplImage->widthStep, colorSpace, kCGImageAlphaPremultipliedLast|kCGBitmapByteOrderDefault);
+    CGContextDrawImage(contextRef, CGRectMake(0, 0, image.size.width, image.size.height), imageRef);
+    CGContextRelease(contextRef);
+    CGColorSpaceRelease(colorSpace);
+    
+    IplImage *ret = cvCreateImage(cvGetSize(iplImage), IPL_DEPTH_8U, 3);
+    cvCvtColor(iplImage, ret, CV_RGB2BGR);
+    cvReleaseImage(&iplImage);
+    
+    return ret;
+}
+
+
+#pragma mark - 暂时用不到
+
+// 单通道彩色图片的直方图
++ (double)CompareHistSignle:(IplImage*)image1 withParam2:(IplImage*)image2 {
+    int hist_size = 256;
+    CvHistogram *gray_hist = cvCreateHist(1, &hist_size, CV_HIST_ARRAY);
+    cvCalcHist(&image1, gray_hist);
+    
+    CvHistogram *gray_hist2 = cvCreateHist(1, &hist_size, CV_HIST_ARRAY);
+    cvCalcHist(&image2, gray_hist2);
+    
+    return cvCompareHist(gray_hist, gray_hist2, CV_COMP_BHATTACHARYYA);
+}
+
+// 进行肤色检测
++ (void)SkinDetect:(IplImage*)src withParam:(IplImage*)dst {
+    // 创建图像头
+    // 用于存图像的一个中间变量，是用来分通道用的，分成hsv通道
+    IplImage* hsv = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 3);
+    // 通道的中间变量，用于肤色检测的中间变量
+    IplImage* tmpH1 = cvCreateImage( cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* tmpS1 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* tmpH2 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* tmpS2 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* tmpH3 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* tmpS3 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* H = cvCreateImage( cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* S = cvCreateImage( cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* V = cvCreateImage( cvGetSize(src), IPL_DEPTH_8U, 1);
+    IplImage* src_tmp1=cvCreateImage(cvGetSize(src),8,3);
+    
+    // 高斯模糊
+    cvSmooth(src,src_tmp1,CV_GAUSSIAN,3,3);
+    
+    // hue色度，saturation饱和度，value纯度
+    cvCvtColor(src_tmp1, hsv, CV_BGR2HSV );
+    // 分为3个通道
+    cvSplit(hsv,H,S,V,0);
+    
+    /*********************肤色检测部分**************/
+    cvInRangeS(H,cvScalar(0.0,0.0,0,0),cvScalar(20.0,0.0,0,0),tmpH1);
+    cvInRangeS(S,cvScalar(75.0,0.0,0,0),cvScalar(200.0,0.0,0,0),tmpS1);
+    cvAnd(tmpH1,tmpS1,tmpH1,0);
+    
+    // Red Hue with Low Saturation
+    // Hue 0 to 26 degree and Sat 20 to 90
+    cvInRangeS(H,cvScalar(0.0,0.0,0,0),cvScalar(13.0,0.0,0,0),tmpH2);
+    cvInRangeS(S,cvScalar(20.0,0.0,0,0),cvScalar(90.0,0.0,0,0),tmpS2);
+    cvAnd(tmpH2,tmpS2,tmpH2,0);
+    
+    // Red Hue to Pink with Low Saturation
+    // Hue 340 to 360 degree and Sat 15 to 90
+    cvInRangeS(H,cvScalar(170.0,0.0,0,0),cvScalar(180.0,0.0,0,0),tmpH3);
+    cvInRangeS(S,cvScalar(15.0,0.0,0,0),cvScalar(90.,0.0,0,0),tmpS3);
+    cvAnd(tmpH3,tmpS3,tmpH3,0);
+    
+    // Combine the Hue and Sat detections
+    cvOr(tmpH3,tmpH2,tmpH2,0);
+    cvOr(tmpH1,tmpH2,tmpH1,0);
+    cvCopy(tmpH1,dst);
+    cvReleaseImage(&hsv);
+    cvReleaseImage(&tmpH1);
+    cvReleaseImage(&tmpS1);
+    cvReleaseImage(&tmpH2);
+    cvReleaseImage(&tmpS2);
+    cvReleaseImage(&tmpH3);
+    cvReleaseImage(&tmpS3);
+    cvReleaseImage(&H);
+    cvReleaseImage(&S);
+    cvReleaseImage(&V);
+    cvReleaseImage(&src_tmp1);
+}
+
+// IplImage类型转换为UIImage类型
++ (UIImage*)convertToUIImage:(IplImage*)image {
+    cvCvtColor(image, image, CV_BGR2RGB);
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    NSData *data = [NSData dataWithBytes:image->imageData length:image->imageSize];
+    CGDataProviderRef provider = CGDataProviderCreateWithCFData((CFDataRef)data);
+    CGImageRef imageRef = CGImageCreate(image->width, image->height, image->depth, image->depth * image->nChannels, image->widthStep, colorSpace, kCGImageAlphaNone | kCGBitmapByteOrderDefault, provider, NULL, false, kCGRenderingIntentDefault);
+    
+    UIImage *ret = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(colorSpace);
+    return ret;
+}
+
+
+
+// 获取相似度
++ (float)isImageFloat:(UIImage *)image1 likeImage:(UIImage *)image2 {
+    IplImage *iplimage1 = [self convertToIplImage:image1];
+    IplImage *iplimage2 = [self convertToIplImage:image2];
+    double sililary = [self ComparePPKHist:iplimage1 withParam2:iplimage2];
+    return sililary;
 }
 
 + (double)CompareHistWithSmallWidthIpl:(IplImage*)srcIpl withBigWidthIplImg:(IplImage*)srcIpl1 {
@@ -219,125 +347,5 @@
     return dbRst;
 }
 
-// 多通道彩色图片的直方图比对
-+ (double)CompareHist:(IplImage*)image1 withParam2:(IplImage*)image2 {
-    int hist_size = 256;
-    IplImage *gray_plane = cvCreateImage(cvGetSize(image1), 8, 1);
-    cvCvtColor(image1, gray_plane, CV_BGR2GRAY);
-    
-    CvHistogram *gray_hist = cvCreateHist(1, &hist_size, CV_HIST_ARRAY);
-    cvCalcHist(&gray_plane, gray_hist);
-    
-    IplImage *gray_plane2 = cvCreateImage(cvGetSize(image2), 8, 1);
-    cvCvtColor(image2, gray_plane2, CV_BGR2GRAY);
-    CvHistogram *gray_hist2 = cvCreateHist(1, &hist_size, CV_HIST_ARRAY);
-    cvCalcHist(&gray_plane2, gray_hist2);
-
-    return cvCompareHist(gray_hist, gray_hist2, CV_COMP_BHATTACHARYYA);
-}
-
-// 单通道彩色图片的直方图
-+ (double)CompareHistSignle:(IplImage*)image1 withParam2:(IplImage*)image2 {
-    int hist_size = 256;
-    CvHistogram *gray_hist = cvCreateHist(1, &hist_size, CV_HIST_ARRAY);
-    cvCalcHist(&image1, gray_hist);
-    
-    CvHistogram *gray_hist2 = cvCreateHist(1, &hist_size, CV_HIST_ARRAY);
-    cvCalcHist(&image2, gray_hist2);
-    
-    return cvCompareHist(gray_hist, gray_hist2, CV_COMP_BHATTACHARYYA);
-}
-
-// 进行肤色检测
-+ (void)SkinDetect:(IplImage*)src withParam:(IplImage*)dst {
-    // 创建图像头
-    // 用于存图像的一个中间变量，是用来分通道用的，分成hsv通道
-    IplImage* hsv = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 3);
-    // 通道的中间变量，用于肤色检测的中间变量
-    IplImage* tmpH1 = cvCreateImage( cvGetSize(src), IPL_DEPTH_8U, 1);
-    IplImage* tmpS1 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
-    IplImage* tmpH2 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
-    IplImage* tmpS2 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
-    IplImage* tmpH3 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
-    IplImage* tmpS3 = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
-    IplImage* H = cvCreateImage( cvGetSize(src), IPL_DEPTH_8U, 1);
-    IplImage* S = cvCreateImage( cvGetSize(src), IPL_DEPTH_8U, 1);
-    IplImage* V = cvCreateImage( cvGetSize(src), IPL_DEPTH_8U, 1);
-    IplImage* src_tmp1=cvCreateImage(cvGetSize(src),8,3);
-    
-    // 高斯模糊
-    cvSmooth(src,src_tmp1,CV_GAUSSIAN,3,3);
-    
-    // hue色度，saturation饱和度，value纯度
-    cvCvtColor(src_tmp1, hsv, CV_BGR2HSV );
-    // 分为3个通道
-    cvSplit(hsv,H,S,V,0);
-    
-    /*********************肤色检测部分**************/
-    cvInRangeS(H,cvScalar(0.0,0.0,0,0),cvScalar(20.0,0.0,0,0),tmpH1);
-    cvInRangeS(S,cvScalar(75.0,0.0,0,0),cvScalar(200.0,0.0,0,0),tmpS1);
-    cvAnd(tmpH1,tmpS1,tmpH1,0);
-    
-    // Red Hue with Low Saturation
-    // Hue 0 to 26 degree and Sat 20 to 90
-    cvInRangeS(H,cvScalar(0.0,0.0,0,0),cvScalar(13.0,0.0,0,0),tmpH2);
-    cvInRangeS(S,cvScalar(20.0,0.0,0,0),cvScalar(90.0,0.0,0,0),tmpS2);
-    cvAnd(tmpH2,tmpS2,tmpH2,0);
-    
-    // Red Hue to Pink with Low Saturation
-    // Hue 340 to 360 degree and Sat 15 to 90
-    cvInRangeS(H,cvScalar(170.0,0.0,0,0),cvScalar(180.0,0.0,0,0),tmpH3);
-    cvInRangeS(S,cvScalar(15.0,0.0,0,0),cvScalar(90.,0.0,0,0),tmpS3);
-    cvAnd(tmpH3,tmpS3,tmpH3,0);
-    
-    // Combine the Hue and Sat detections
-    cvOr(tmpH3,tmpH2,tmpH2,0);
-    cvOr(tmpH1,tmpH2,tmpH1,0);
-    cvCopy(tmpH1,dst);
-    cvReleaseImage(&hsv);
-    cvReleaseImage(&tmpH1);
-    cvReleaseImage(&tmpS1);
-    cvReleaseImage(&tmpH2);
-    cvReleaseImage(&tmpS2);
-    cvReleaseImage(&tmpH3);
-    cvReleaseImage(&tmpS3);
-    cvReleaseImage(&H);
-    cvReleaseImage(&S);
-    cvReleaseImage(&V);
-    cvReleaseImage(&src_tmp1);
-}
-
-// UIImage类型转换为IPlImage类型
-+ (IplImage*)convertToIplImage:(UIImage*)image {
-    CGImageRef imageRef = image.CGImage;
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    IplImage *iplImage = cvCreateImage(cvSize(image.size.width, image.size.height), IPL_DEPTH_8U, 4);
-    CGContextRef contextRef = CGBitmapContextCreate(iplImage->imageData, iplImage->width, iplImage->height, iplImage->depth, iplImage->widthStep, colorSpace, kCGImageAlphaPremultipliedLast|kCGBitmapByteOrderDefault);
-    CGContextDrawImage(contextRef, CGRectMake(0, 0, image.size.width, image.size.height), imageRef);
-    CGContextRelease(contextRef);
-    CGColorSpaceRelease(colorSpace);
-    
-    IplImage *ret = cvCreateImage(cvGetSize(iplImage), IPL_DEPTH_8U, 3);
-    cvCvtColor(iplImage, ret, CV_RGB2BGR);
-    cvReleaseImage(&iplImage);
-    
-    return ret;
-}
-
-// IplImage类型转换为UIImage类型
-+ (UIImage*)convertToUIImage:(IplImage*)image {
-    cvCvtColor(image, image, CV_BGR2RGB);
-    
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    NSData *data = [NSData dataWithBytes:image->imageData length:image->imageSize];
-    CGDataProviderRef provider = CGDataProviderCreateWithCFData((CFDataRef)data);
-    CGImageRef imageRef = CGImageCreate(image->width, image->height, image->depth, image->depth * image->nChannels, image->widthStep, colorSpace, kCGImageAlphaNone | kCGBitmapByteOrderDefault, provider, NULL, false, kCGRenderingIntentDefault);
-    
-    UIImage *ret = [UIImage imageWithCGImage:imageRef];
-    CGImageRelease(imageRef);
-    CGDataProviderRelease(provider);
-    CGColorSpaceRelease(colorSpace);
-    return ret;
-}
 
 @end
