84e4e2c626043fcac3b2eba137f1c6f05c2375be
YuChao529258668 <529258668@qq.com>
2020-11-30 11:05:48 +0800

    增加自定义底部栏

---
+++ YCPictureManager/YCPictureManager/YCBottomBar.h
@@ -0,0 +1,16 @@
+//
+//  YCBottomBar.h
+//  YCPictureManager
+//
+//  Created by 余超 on 2020/11/28.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YCBottomBar : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YCPictureManager/YCPictureManager/YCBottomBar.m
@@ -0,0 +1,20 @@
+//
+//  YCBottomBar.m
+//  YCPictureManager
+//
+//  Created by 余超 on 2020/11/28.
+//
+
+#import "YCBottomBar.h"
+
+@implementation YCBottomBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
